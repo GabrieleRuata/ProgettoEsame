02ba0d9f7fdf70a712048fd6f99f4e18f0f894d1
Gabriele Ruata <g.ruata.1371@vallauri.edu>
2022-04-14 10:06:38 +0200

    btn full screen

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $('document').ready(function () {
   const Rain = new Audio("Reference/Rain.mp3");
   const Fire = new Audio("Reference/Fire.mp3");
   const Music = new Audio("Reference/The Central Park North - All the Time.mp3");
+  $('.exitfullscreen').hide();
 
 
   $('#play').click(function () {
@@ -28,5 +29,32 @@ $('document').ready(function () {
   $('#stopP').click(function () {
       Music.pause();
   });
+
+  $('.requestfullscreen').click(function () {
+    var elem = document.documentElement;
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+    $('.requestfullscreen').hide();
+    $('.exitfullscreen').show();
+  });
+
+  $('.exitfullscreen').click(function () {
+    var elem = document.documentElement;
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+    $('.exitfullscreen').hide();
+    $('.requestfullscreen').show();
+  });
+
 });
 
